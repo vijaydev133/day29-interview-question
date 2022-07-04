e73da1f397e44e8c2eb37a683c0afeabac2e9755
vijaydev133 <vijaydeveloper004@gmail.com>
2022-07-04 21:54:08 +0530

    Adding Code

---
+++ main.js
@@ -0,0 +1,37 @@
+/**************************************************************************** */
+// 1.How to link css files
+//Ans) We can link css file through link tag with href attribute in head. 
+
+/**************************************************************************** */
+// 2.Display: visibility
+//Ans)display	Specifies how an element should be displayed
+//visibility	Specifies whether or not an element should be visible
+
+/**************************************************************************** */
+// 3.anonymous function in javascript with example
+//Ans)Anonymous function which don't have a name, which can be called by assigning the
+//function definition to a variable. and call the function through the variable 
+
+/**************************************************************************** */
+// 4. Splice and slice difference and what it will returns
+//Ans)Slice will slice a part of the array and return the sliced part
+//Splice can be used to cut, copy and delete the part of or single element in array
+
+/**************************************************************************** */
+// 5.  Spread operator
+//Ans) Spread operator can be used to spread the elements in the array,
+//Spread is also used to Destructure array and object
+
+/**************************************************************************** */
+// 6. Difference Between local storage and session storage
+//Ans)Local storage in where we can store short amount of data, which will be 
+//availabe even after the page gets refreshed
+// but, session storage will available only for that session, i.e, data will be
+// deleted when it is refreshed
+
+
+let arr = [1,4,7,9,6,4]
+
+let arr1 = arr.slice(0,3)
+
+console.log(arr1);
